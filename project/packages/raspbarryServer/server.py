41e8d086ce7449cdb5c0ab1ab17ac69a35af84b5
Christian Alexsander <christianalexbh@hotmail.com>
2020-06-24 21:39:08 -0300

    encode and no write

--- project/packages/raspbarryServer/server.py
+++ project/packages/raspbarryServer/server.py
@@ -19,7 +19,7 @@ class ServerRaspy:
     def sendMensage(self, mensage: str, type):
         jsonData = json.dumps({"mensage": mensage, "type": type})
         mensageJson = '{jsonData};'.format(jsonData=jsonData)
-        self.socketInstance.send(bytes(mensageJson, encoding='utf-8'))
+        self.socketInstance.send(str.encode(mensageJson))
 
 
 serverInstance = ServerRaspy()
