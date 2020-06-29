cda0debd5a093d0174496f75ff511b6609be9dbb
Christian Alexsander <christianalexbh@hotmail.com>
2020-06-29 20:22:47 -0300

    server 2

--- project/packages/raspbarryServer/sonar.py
+++ project/packages/raspbarryServer/sonar.py
@@ -42,8 +42,6 @@ GPIO.setup(ECHO, GPIO.IN)
 # Inicializa TRIG em nível lógico baixo
 GPIO.output(TRIG, False)
 time.sleep(1)
-print('distnacia teste')
-serverInstance.sendMensage(80, 'distancia')
 
 print("Sampling Rate:", sampling_rate, "Hz")
 print("Distances (cm)")
