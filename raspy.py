d118dca188559cd6ea24443a26131aba906de5d6
Christian Alexsander <christianalexbh@hotmail.com>
2020-06-30 19:36:13 -0300

    serve

--- raspy.py
+++ raspy.py
@@ -1,6 +1,30 @@
 import project.packages.env
 from project.packages.raspbarryServer import server
 
-
-for i in range(10):
-    server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
+server.sendMensage('testes', 'teste')
